1dacadad9b79024992eafc578597549c7f5392e0
HS <hs@HSs-MacBook-Pro.local>
2022-10-10 14:35:44 +0100

    added navigation buttons between homescreen and single foodbank

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:open_pantry/pages/single_foodbank.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
- @override
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
-    child: Scaffold(
-      body: Center(
-      child: ElevatedButton(
-      onPressed: (){
-        Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => SingleFoodbank())
-        );
-      },
-      child: Text("Go to next page")
-    ),
-    ),
-    ),
+      child: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SingleFoodbank()));
+              },
+              child: Text("Go to next page")),
+        ),
+      ),
     );
   }
 }
---
+++ lib/pages/single_foodbank.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class SingleFoodbank extends StatelessWidget {
+  const SingleFoodbank({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: ElevatedButton(
+            child: Text('Go back'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            }),
+      ),
+    );
+  }
+}
