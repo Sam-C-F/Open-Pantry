0b4c1941f76d086c80a071254e958400980a5258
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-10-12 16:27:59 +0100

    edited url call in api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -7,20 +7,9 @@ import 'package:http/http.dart' as http;
 const baseUrl = "https://www.givefood.org.uk";
 
 class API {
-  static Future getUsers() {
-    var url = baseUrl + "/api/2/foodbanks/";
+  static Future getUsersByLocation(location) {
+    var url = baseUrl + "/api/2/foodbanks/search/?lat_lng=${location}";
     var parsedUrl = Uri.parse(url);
     return http.get(parsedUrl);
   }
-
-  
 }
-
-// class API {
-//   static Future getUsersByLocation(location) {
-//     var url = baseUrl + "/api/2/foodbanks/search/?lat_lng=${location}";
-//     var parsedUrl = Uri.parse(url);
-//     return http.get(parsedUrl);
-//   }
-// }
-
