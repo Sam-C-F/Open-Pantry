4b322750a49449e24904614e5c8e57f04697598b
tommarsh27 <108725749+tommarsh27@users.noreply.github.com>
2022-10-17 12:43:48 +0100

    map working list not

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
