e26eb0f9d146a2d828caed8ffb53c2cc07a5546c
caitlin-fin <c8linfin@gmail.com>
2022-10-18 10:42:17 +0100

    add location check to api.dart geolocator with async

--- lib/api.dart
+++ lib/api.dart
@@ -7,7 +7,12 @@ import 'package:http/http.dart' as http;
 const baseUrl = "https://www.givefood.org.uk";
 
 class API {
-  static Future getUsersByLocation(location) {
+  static Future getUsersByLocation(location) async {
+    bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    await Geolocator.checkPermission();
+    await Geolocator.requestPermission();
+
     var url = baseUrl + "/api/2/foodbanks/search/?lat_lng=${location}";
     var parsedUrl = Uri.parse(url);
     return http.get(parsedUrl);
