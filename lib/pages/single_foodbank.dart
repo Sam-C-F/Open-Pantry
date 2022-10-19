0623b770195696fa7b4b11355ba10f5b39cf2e2c
NRMSMITH <niamhrmsmith94@hotmail.co.uk>
2022-10-19 11:20:42 +0100

    styling for single fb page

--- lib/pages/single_foodbank.dart
+++ lib/pages/single_foodbank.dart
@@ -36,8 +36,7 @@ void _onMapCreated(GoogleMapController controller) {
 //   return latlon;
 // }
 
-
-  _fbLocation(passedLatLng)  {
+_fbLocation(passedLatLng) {
   var currentLocation = passedLatLng;
   var splitLocation = currentLocation.split(",");
   var lat = double.parse(splitLocation[0]);
@@ -77,76 +76,68 @@ class _MyWidgetState extends State<FoodBank> {
   Widget build(BuildContext context) {
     final List<String> needsList = widget.passedNeeds.split('\n');
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         title: 'FoodBank',
         home: Scaffold(
             backgroundColor: Color(0xffFDF5E6),
             appBar: AppBar(
                 backgroundColor: Color(0xffFDF5E6),
-
                 title: Row(
                   children: [
                     Expanded(
-                      flex: 1,
-                      child: Image.asset('assets/images/picwish.png'),
-                    ),
-
-
-                    Expanded(
-                      flex: 4,
-                      child: Text('${widget.passedName}',
-                          style: TextStyle(
-                              fontFamily: 'Josefin',
-                              fontSize: 50,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xff79b465))),
+                      child:
+                          Image.asset('assets/images/grouplogo.png', scale: 2),
                     ),
                   ],
                 ),
                 leading: ElevatedButton(
-                    child: Icon(Icons.arrow_back),
+                  
+                    child: Icon(Icons.home, color: Color(0xff79b465)),
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xff79b465)),
+                        backgroundColor: Color(0xffFDF5E6)),
                     onPressed: () {
                       Navigator.of(context).pop();
                     })),
             body: Column(children: [
-              Row(children: [
-                Container(
-                  
-                  child: Expanded(
-                  // color:Color.fromARGB(255, 180, 240, 182),
-                  // margin: const EdgeInsets.all(8),
-                  // width: 2000.0,
-                  child: Column(
-                    children: [
-                      Text('\n${widget.passedUrls}\n',
-                          style: TextStyle(fontSize: 20)),
-                      Text('${widget.passedAddress}',
-                          style: TextStyle(fontSize: 15)),
-                    ],
-                  ),
-                ),),
-                Container(
-                  constraints: const BoxConstraints(maxHeight: 300, maxWidth: 300),
-                  child: Expanded(
-                  child: GoogleMap(
-                    onMapCreated: _onMapCreated,
-                    
-                    initialCameraPosition:
-                        CameraPosition(target: _fbLocation(widget.passedLatLng)),
-                  ),)
-
-                )
-              ]),
               Container(
-                  color: Color(0xff79b465),
+                  color: Color(0xFFFDF5E6),
                   alignment: Alignment.center,
                   padding: EdgeInsets.all(6),
                   child: Text(
-                    'Needs',
+                    '${widget.passedName}',
                     style: TextStyle(
                         fontFamily: 'Josefin',
-                        color: Color(0xFFFDF5E6),
+                        color: Color(0xff79b465),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  )),
+              Container(
+                  child: Expanded(
+                child: GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                        target: _fbLocation(widget.passedLatLng), zoom: 15.0),
+                    markers: Set<Marker>.of([
+                      Marker(
+                        markerId: MarkerId(widget.passedName),
+                        position: _fbLocation(widget.passedLatLng),
+                        infoWindow: InfoWindow(
+                          title: widget.passedName,
+                          snippet: widget.passedAddress,
+                        ),
+                      )
+                    ])),
+              )),
+              Container(
+                  color: Color(0xFFFDF5E6),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(6),
+                  margin: EdgeInsets.only(bottom: 4),
+                  child: Text(
+                    'Current Requested Items',
+                    style: TextStyle(
+                        fontFamily: 'Josefin',
+                        color: Color(0xff79b465),
                         fontSize: 30,
                         fontWeight: FontWeight.bold),
                   )),
